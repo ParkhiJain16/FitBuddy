068ddf6aee391c17fcf3c34f625508d60f094fb4
Test <parkhijain1628@gmail.com>
2026-04-29 16:27:43 +0530

    clean aicontroller

--- backend/controller/aiController.js
+++ backend/controller/aiController.js
@@ -781,7 +781,7 @@ exports.askAI = async (req, res) => {
 
   try {
     const response = await axios.post(
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent",
+      "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent",
       {
         contents: [
           {
